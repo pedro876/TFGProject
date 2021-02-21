34c04deed5f4d44b263e0cabbeb66a14f234c93a
pedro876 <pedrocasas876@gmail.com>
2021-02-21 14:21:21 +0100

    project

---
+++ Assets/Scripts/UI/PanelResize.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PanelResize : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    //Button btn;
+    const string resizeCursor = "resizeCursor";
+    Texture2D resizeCursorTex;
+
+    bool drag = false;
+
+    [SerializeField] RectTransform panel;
+
+    void Start()
+    {
+        //btn = GetComponent<Button>();
+        resizeCursorTex = Resources.Load("Cursors/resizeCursor") as Texture2D;
+    }
+    
+    void Update()
+    {
+        if (drag)
+        {
+            float x = Input.mousePosition.x;
+            if(panel != null)
+            {
+                float size = x - panel.rect.xMin;
+                panel.sizeDelta = new Vector2(size, panel.sizeDelta.y);
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("pointer enter");
+        Cursor.SetCursor(resizeCursorTex, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("pointer exit");
+        if(!drag)
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("pointer down");
+        drag = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("pointer up");
+        drag = false;
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
---
+++ Assets/Scripts/UI/ViewSizeFixed.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class ViewSizeFixed : MonoBehaviour
+{
+    [SerializeField] RectTransform functionPanel;
+    RectTransform rectTransform;
+    AspectRatioFitter fitter;
+    /*[SerializeField] */RectTransform parentRectTransform;
+
+    [ExecuteAlways]
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        fitter = GetComponent<AspectRatioFitter>();
+        parentRectTransform = transform.parent.GetComponent<RectTransform>();
+    }
+
+    [ExecuteAlways]
+    void Update()
+    {
+        if (functionPanel == null || rectTransform == null || fitter == null) return;
+
+        float parentWidth = parentRectTransform ? parentRectTransform.rect.width : Screen.width;
+        float parentHeight = parentRectTransform ? parentRectTransform.rect.height : Screen.height;
+
+        float xMax = functionPanel.rect.xMax;
+        float width = parentWidth - xMax;
+
+        
+
+        fitter.aspectMode = parentHeight > width ?
+            AspectRatioFitter.AspectMode.HeightControlsWidth : 
+            AspectRatioFitter.AspectMode.WidthControlsHeight;
+
+        rectTransform.sizeDelta = new Vector2(width, parentHeight);
+
+        rectTransform.anchoredPosition = new Vector2(
+            xMax + (width - parentWidth) * 0.5f,
+            rectTransform.anchoredPosition.y);
+    }
+}
